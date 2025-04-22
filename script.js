dc8fd7a9867f7b9de35f7ad02ef934cbbd3b0bce
yuve <c.opy.qp@gmail.com>
2025-04-22 22:56:30 +0800

    part finished

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const API_KEY = "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";
+const API_KEY = "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";
+
+const content = document.getElementById('content');
+const chatInput = document.getElementById('ChatInput');
+const sendButton = document.getElementById('SendButton');
+
+sendButton.addEventListener('click', () => handleSendMessage());
+chatInput.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+        handleSendMessage();
+    }
+})
+
+function handleSendMessage()
+{
+    const question = chatInput.value.trim();
+    
+    addQuestonSection(question);
+}
 
 const fetchData =
     fetch("https://openrouter.ai/api/v1/chat/completions", {
@@ -8,20 +26,32 @@ const fetchData =
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "model": "openai/o4-mini",
+            "model": "deepseek/deepseek-r1-distill-llama-8b",
             "messages": [
                 {
                     "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": "Hi, do you need help?"
-                        },
-                    ]
+                    "content": "i need help with my studies"
                 }
             ]
         })
     });
 
-    fetchData.then(response => response.json())
-        .then(data => console.log(data))
+// fetchData.then(response => response.json())
+//     .then(data => console.log(data.choices[0].message.content))
+
+function addQuestonSection(message)
+{
+    const sectionElement = document.createElement('section');
+    sectionElement.className = 'question-section';
+    sectionElement.textContent = message;
+
+    content.appendChild(sectionElement);
+    addAnswerSection(message)
+}
+
+function addAnswerSection(message)
+{
+    
+}
+
+
